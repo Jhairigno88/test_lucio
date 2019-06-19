3a3aa703231aad35fc58dec679f6cefa9ef94b04
J41r1gn0 <alefin.88@gmail.com>
2019-06-20 00:02:02 +0200

    prova 20-6-19 ore 00:02

--- test_aTreTabelle/src/sincrono/corso/test_aTreTabelle/main/Main.java
+++ test_aTreTabelle/src/sincrono/corso/test_aTreTabelle/main/Main.java
@@ -20,19 +20,14 @@ public class Main {
 		Persona p=new Persona();
 		Motorino m= new Motorino();
 		Automobile a=new Automobile();
-		File A =new File("prova.txt");
-		File M=new File("Mezzo_di_locomozione-txt");
-		File C=new File("Connecting_people-txt");
+		File A=new File("prova.txt");
+		File M=new File("Mezzo_di_locomozione.txt");
+		File C=new File("Connecting_people.txt");
 	
 		if( A.exists()){
 			System.out.println("il File già esiste!");
 		}		
 		else {A.createNewFile();
-		
-							
-//			PrintWriter scrivi= new PrintWriter(A);
-//			scrivi.println("Anagrafiche");
-//			scrivi.close();
 			System.out.println("creato!");			 
 		}
 		
@@ -49,10 +44,16 @@ public class Main {
 		else {C.createNewFile();
 			System.out.println("creato!");
 		}
+		/*
+		System.out.println("Ciao, per prima cosa scrivi ");
+		System.out.println("il numero di persone che vuoi inserire");
+		int x=scn.nextInt();
+		*/
 		
 		ArrayList<Persona> persone= new ArrayList<Persona>() ;
-			for (int i=0;i<2;i++) {
 			
+			for (int i=0;i<1;i++) {
+			System.out.println(i+1);
 			System.out.println("inserisci il nome di una persona!");
 			p.setNome(scn.nextLine());
 			
@@ -64,30 +65,43 @@ public class Main {
 			
 			System.out.println("inserisci l'indirizzo del suo domicilio");
 			p.setNome(scn.nextLine());
+			persone.add(p);
+			
 			
 			System.out.println("questa persona ha un mezzo di locomozione?");
 			String risp= scn.nextLine();
 			
 			if(risp.equals("si")) {
 				System.out.println("che tipo di mezzo è? un motorino o una macchina?");
-				String ri=scn.next();
+				String ri=scn.nextLine();
 				if (ri.equals("motorino")) {
-					System.out.println("inserisci la marca del mezzo");
-					m.setMarca(scn.nextLine());
+					System.out.println("inserisci il modello del mezzo");
+					m.setModello(scn.nextLine());
 					System.out.println("e la sua targa:");
 					m.setTarga(scn.nextLine());
 				}
 				else {
-					System.out.println("inserisci la marca del mezzo");
-					a.setMarca(scn.nextLine());
+					System.out.println("inserisci il modello del mezzo");
+					a.setModello(scn.nextLine());
 					System.out.println("e la sua targa:");
 					a.setTarga(scn.nextLine());
 				}
 				
 			}
 			
+			PrintWriter scrivi= new PrintWriter(A);
+			scrivi.println("Elenco Persone");
+			scrivi.println("nome: " + p.GetNome() + " Cognome: " + p.getCognome() + 
+							" Codice fiscale: " + p.getCodicefiscale() + 
+							" indirizzo: " + p.getIndirizzo());
+			scrivi.close();
 		
 			
+			PrintWriter scrivim=new PrintWriter(M);
+			scrivim.println("Elenco mezzi di locomozione");
+			scrivim.println("Tipo di mezzo: " + m.getModello() + " Targa: " + m.getTarga());
+			scrivim.close();
+		
 			
 			
 			
